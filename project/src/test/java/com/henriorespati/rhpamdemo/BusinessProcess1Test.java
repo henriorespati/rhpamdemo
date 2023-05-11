b43c3577b8c4405a512d3228375d40c085ad9807
“hrespati” <hrespati@redhat.com>
2023-05-11 17:21:55 +0700

    Initial commit

---
+++ project/src/test/java/com/henriorespati/rhpamdemo/BusinessProcess1Test.java
@@ -0,0 +1,14 @@
+package com.henriorespati.rhpamdemo;
+
+import org.jbpm.test.*;
+import org.junit.jupiter.api.*;
+
+public class BusinessProcess1Test extends JbpmJUnitBaseTestCase {
+    public BusinessProcess1Test() {
+        super(true, true);
+    }
+
+    @Test
+    public void testProcess() {
+    }
+}
